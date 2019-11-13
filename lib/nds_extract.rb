786786c4533fa87c54692499c3faa4d38e866c0d
Isaac Avilez <isaac1180@aol.com>
2019-11-13 13:13:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,9 +9,9 @@ def directors_totals(nds)
   result = {
     
   }
-  director_index = 0
-  while director_index < nds.size do
-    director = nds[director_index]
+  dir_index = 0
+  while dir_index < nds.length do
+    director = nds[dir_index]
     result[director[:name]] = gross_for_director(director)
     director_index += 1
   end

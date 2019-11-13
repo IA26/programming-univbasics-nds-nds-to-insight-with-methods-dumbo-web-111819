ebb0240559e45105f1327e62f6dcdd01b70558d5
Isaac Avilez <isaac1180@aol.com>
2019-11-13 13:21:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,7 +20,7 @@ end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
+def gross_for_director(dir_data)
   total = 0
   counter = 0
 
